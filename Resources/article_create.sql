eadfffef6e8c3e5e02ffbed352fbef26333784b5
ogdabou <ogdabou@gmail.com>
2014-07-01 17:39:23 +0200

    Updated database + Refactoring

---
+++ Resources/article_create.sql
@@ -0,0 +1,30 @@
+﻿-- Table: articles
+
+-- DROP TABLE articles;
+
+CREATE TABLE articles
+(
+  id_article bigint NOT NULL DEFAULT nextval('"Articles_id_articles_seq"'::regclass),
+  titre_article character varying(200),
+  contenu_article text,
+  couv_article character varying(100),
+  description text,
+  id_auteur integer,
+  id_categorie integer,
+  url character varying(500),
+  creation_date timestamp with time zone DEFAULT now(),
+  publication_date character varying(30),
+  sources text,
+  CONSTRAINT pk_article PRIMARY KEY (id_article),
+  CONSTRAINT fk_auteur_article FOREIGN KEY (id_auteur)
+      REFERENCES "Auteur" (id_auteur) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_categorie FOREIGN KEY (id_categorie)
+      REFERENCES "Categorie" (id_categorie) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE articles
+  OWNER TO postgres;
--- src/main/java/giveme/controllers/ItemController.java
+++ src/main/java/giveme/controllers/ItemController.java
@@ -3,6 +3,7 @@ package giveme.controllers;
 import giveme.common.dao.ArticleDao;
 import giveme.common.dao.AuthorDao;
 import giveme.common.dao.CategorieDao;
+import giveme.common.services.ArticleServices;
 
 import java.io.IOException;
 
@@ -26,6 +27,9 @@ public class ItemController
 	@Autowired
 	private AuthorDao		authordao;
 
+	@Autowired
+	private ArticleServices	articleServices;
+
 	/**
 	 *
 	 */
@@ -41,63 +45,6 @@ public class ItemController
 	@RequestMapping(value = "/update/items", method = RequestMethod.GET)
 	public void updateItemsApi() throws IOException
 	{
-		// List<Article> articles = findArticles();
-		//
-		// List<Article> ar = articleDao.list();
-		// for (Article article : ar)
-		// {
-		// LOGGER.info(article.getTitle());
-		// }
+		articleServices.refreshAll();
 	}
-
-	// /**
-	// *
-	// * @return
-	// * @throws IOException
-	// */
-	// private List<Article> findArticles() throws IOException
-	// {
-	// List<Article> articleList = new ArrayList<>();
-	// String job = "4002/5/6";
-	// String start = "0";
-	// String end = "10";
-	// String query = "https://storage.scrapinghub.com/items/" + job +
-	// "?apikey=78c8f0b2681f4203a31f5277c2696c88"
-	// + "&start=" + job + "/" + start + "&count=" + end;
-	// // LOGGER.info("query : " + query);
-	// URL url = new URL(query);
-	//
-	// BufferedReader in = new BufferedReader(new
-	// InputStreamReader(url.openStream()));
-	//
-	// String jsonValue;
-	//
-	// List<Article> articles = new ArrayList<>();
-	// while ((jsonValue = in.readLine()) != null)
-	// {
-	// // LOGGER.info("received " + jsonValue);
-	// ObjectMapper mapper = new ObjectMapper();
-	// mapper.configure(Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	//
-	// JsonNode rootNode = mapper.readTree(jsonValue);
-	//
-	// String itemType = extractItemType(rootNode);
-	//
-	// if (ARTICLE_DETAILS.getType().equals(itemType))
-	// {
-	// articles = extractDetails(jsonValue, mapper);
-	//
-	// }
-	// else if (ARTICLE.getType().equals(itemType))
-	// {
-	// Article article = extractArticle(jsonValue, mapper);
-	// articleList.add(article);
-	// }
-	// }
-	//
-	// articleList = mergeDetailsAndContent(articleList, articles);
-	// in.close();
-	// return articleList;
-	// }
-
 }
